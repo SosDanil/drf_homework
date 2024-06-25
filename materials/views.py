27a880ceabf1484c8ca8558df6214f79e9aee636
Danil Klimenko <sos.danil@yandex.ru>
2024-06-26 01:44:34 +0500

    25.2 - сделал задание 4 с разделением на классы по правам

--- materials/views.py
+++ materials/views.py
@@ -63,7 +63,7 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [~IsModer, IsOwner]
+    permission_classes = [~IsModer, IsOwner, IsAuthenticated]
 
 
 class SetSubscribe(APIView):
