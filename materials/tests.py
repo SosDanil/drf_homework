8693cebf0a0e2e24d52de385504ff9159f157e77
Danil Klimenko <sos.danil@yandex.ru>
2024-06-26 01:47:03 +0500

    25.2 - сделал задание 4 с разделением на классы по правам

--- materials/tests.py
+++ materials/tests.py
@@ -7,90 +7,90 @@ from materials.models import Course, Lesson, Subscribe
 from users.models import User
 
 
-# class LessonTestOwnerNotModerCase(APITestCase):
-#
-#     def setUp(self):
-#         self.user = User.objects.create(pk=1, email='admin@sky.ru', password='qwerty')
-#
-#         self.course = Course.objects.create(pk=1, name='Biology', owner=self.user)
-#         self.lesson = Lesson.objects.create(name='Fauna', url_video='http://video.youtube.com', owner=self.user,
-#                                             course=self.course)
-#         self.client.force_authenticate(user=self.user)
-#
-#     def test_lesson_retrieve(self):
-#         url = reverse('materials:lesson_retrieve', args=(self.lesson.pk,))
-#         response = self.client.get(url)
-#         print('retrieve')
-#         print(response.json())
-#         data = response.json()
-#         self.assertEqual(
-#             response.status_code,
-#             status.HTTP_200_OK
-#         )
-#         self.assertEqual(
-#             data.get('name'),
-#             self.lesson.name
-#         )
-#
-#     def test_lesson_list(self):
-#         url = reverse('materials:lesson_list')
-#         response = self.client.get(url)
-#         print(response.json())
-#         self.assertEqual(
-#             response.status_code,
-#             status.HTTP_403_FORBIDDEN
-#         )
-#
-#     def test_lesson_create(self):
-#         url = reverse('materials:lesson_create')
-#         data = {
-#             "name": "Flora",
-#             "url_video": 'http://video.youtube.com',
-#             "course": 1,
-#             "owner": 1
-#         }
-#         response = self.client.post(url, data=data)
-#         data = response.json()
-#         print(data)
-#         self.assertEqual(
-#             response.status_code,
-#             status.HTTP_201_CREATED
-#         )
-#         self.assertEqual(
-#             response.json(),
-#             {'id': 2, 'name': 'Flora', 'preview': None, 'description': None, 'url_video': 'http://video.youtube.com',
-#              'course': 1, 'owner': 1}
-#         )
-#
-#     def test_lesson_update(self):
-#         url = reverse('materials:lesson_update', args=(self.lesson.pk,))
-#         data = {
-#             "name": "Flora",
-#             "url_video": "http://www.youtube.com"
-#         }
-#         response = self.client.patch(url, data=data)
-#         print(response.json())
-#         self.assertEqual(
-#             data.get('name'),
-#             "Flora"
-#         )
-#         self.assertEqual(
-#             response.status_code,
-#             status.HTTP_200_OK
-#         )
-#
-#     def test_lesson_delete(self):
-#         url = reverse('materials:lesson_delete', args=(self.lesson.pk,))
-#         response = self.client.delete(url)
-#         print(response.json())
-#         self.assertEqual(
-#             Lesson.objects.all().count(),
-#             1
-#         )
-#         self.assertEqual(
-#             response.status_code,
-#             status.HTTP_204_NO_CONTENT
-#         )
+class LessonTestOwnerNotModerCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(pk=1, email='admin@sky.ru', password='qwerty')
+
+        self.course = Course.objects.create(pk=1, name='Biology', owner=self.user)
+        self.lesson = Lesson.objects.create(name='Fauna', url_video='http://video.youtube.com', owner=self.user,
+                                            course=self.course)
+        self.client.force_authenticate(user=self.user)
+
+    def test_lesson_retrieve(self):
+        url = reverse('materials:lesson_retrieve', args=(self.lesson.pk,))
+        response = self.client.get(url)
+        print('retrieve')
+        print(response.json())
+        data = response.json()
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get('name'),
+            self.lesson.name
+        )
+
+    def test_lesson_list(self):
+        url = reverse('materials:lesson_list')
+        response = self.client.get(url)
+        print(response.json())
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_403_FORBIDDEN
+        )
+
+    def test_lesson_create(self):
+        url = reverse('materials:lesson_create')
+        data = {
+            "name": "Flora",
+            "url_video": 'http://video.youtube.com',
+            "course": 1,
+            "owner": 1
+        }
+        response = self.client.post(url, data=data)
+        data = response.json()
+        print(data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            response.json(),
+            {'id': 2, 'name': 'Flora', 'preview': None, 'description': None, 'url_video': 'http://video.youtube.com',
+             'course': 1, 'owner': 1}
+        )
+
+    def test_lesson_update(self):
+        url = reverse('materials:lesson_update', args=(self.lesson.pk,))
+        data = {
+            "name": "Flora",
+            "url_video": "http://www.youtube.com"
+        }
+        response = self.client.patch(url, data=data)
+        print(response.json())
+        self.assertEqual(
+            data.get('name'),
+            "Flora"
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_lesson_delete(self):
+        url = reverse('materials:lesson_delete', args=(self.lesson.pk,))
+        response = self.client.delete(url)
+        print(response.json())
+        self.assertEqual(
+            Lesson.objects.all().count(),
+            1
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
 
 
 class LessonTestModerNotOwnerCase(APITestCase):
